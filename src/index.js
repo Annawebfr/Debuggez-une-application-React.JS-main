ac1664fb4aaadfe4107e95182b0389a0b8e371c2
Anna Hultgren <annahultgren5@gmail.com>
2025-02-03 17:42:42 +0100

    corriger les fautes et istallation

--- src/index.js
+++ src/index.js
@@ -1,69 +1,18 @@
-import PropTypes from "prop-types";
+/* istanbul ignore file */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import "./style.scss";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-export const BUTTON_TYPES = {
-  DEFAULT: 1,
-  SUBMIT: 2,
-};
-
-const Button = ({ title, onClick, type, disabled, children }) => {
-  switch (type) {
-    case BUTTON_TYPES.DEFAULT:
-      return (
-        <button
-          type="button"
-          disabled={disabled}
-          className="Button"
-          data-testid="button-test-id"
-          onClick={onClick}
-          title={title}
-        >
-          {children}
-        </button>
-      );
-    case BUTTON_TYPES.SUBMIT:
-      return (
-        <input
-          disabled={disabled}
-          className="Button"
-          type="submit"
-          data-testid="button-test-id"
-          value={children}
-          onClick={onClick}
-          title={title}
-        />
-      );
-    default:
-      return (
-        <button
-          type="button"
-          disabled={disabled}
-          className="Button"
-          data-testid="button-test-id"
-          onClick={onClick}
-          title={title}
-        >
-          {children}
-        </button>
-      );
-  }
-};
-
-// eslint-disable-next-line react/no-typos
-Button.propTypes = {
-  title: PropTypes.string,
-  onClick: PropTypes.func,
-  type: PropTypes.number,
-  disabled: PropTypes.bool,
-  children: PropTypes.node,
-};
-Button.defaultProps = {
-  disabled: false,
-  onClick: () => null,
-  type: BUTTON_TYPES.DEFAULT,
-  title: "",
-  children: null
-}
-
-export default Button;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
